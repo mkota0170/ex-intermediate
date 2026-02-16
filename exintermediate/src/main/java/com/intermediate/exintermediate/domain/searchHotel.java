dd186d58f59886bed2043ae07156de4de51a0a9d
kotaro <kotaro.matsuhana@rakus-partners.co.jp>
2026-02-16 14:03:03 +0900

    各クラスの編集

--- exintermediate/src/main/java/com/intermediate/exintermediate/domain/searchHotel.java
+++ exintermediate/src/main/java/com/intermediate/exintermediate/domain/searchHotel.java
@@ -1,5 +1,72 @@
 package com.intermediate.exintermediate.domain;
 
+/**
+ * 情報を受け取るためのdomainクラス
+ */
 public class searchHotel {
+    
+    //id
+    private Integer id;
+    //場所名(首都圏)
+    private String areaName;
+    //住所
+    private String address;
+    //ホテル名
+    private String hotelName;
+    //最寄り駅
+    private String nearestStation;
+    //料金
+    private Integer price;
+    //駐車場の有無
+    private String parking;
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getAreaName() {
+        return areaName;
+    }
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getHotelName() {
+        return hotelName;
+    }
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+    public String getNearestStation() {
+        return nearestStation;
+    }
+    public void setNearestStation(String nearestStation) {
+        this.nearestStation = nearestStation;
+    }
+    public Integer getPrice() {
+        return price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    public String getParking() {
+        return parking;
+    }
+    public void setParking(String parking) {
+        this.parking = parking;
+    }
+    @Override
+    public String toString() {
+        return "searchHotel [id=" + id + ", areaName=" + areaName + ", address=" + address + ", hotelName=" + hotelName
+                + ", nearestStation=" + nearestStation + ", price=" + price + ", parking=" + parking + "]";
+    }
+
     
 }
--- exintermediate/src/main/java/com/intermediate/exintermediate/repository/searchHotelRepository.java
+++ exintermediate/src/main/java/com/intermediate/exintermediate/repository/searchHotelRepository.java
@@ -1,8 +1,40 @@
 package com.intermediate.exintermediate.repository;
 
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.intermediate.exintermediate.domain.searchHotel;
 
 @Repository
 public class searchHotelRepository {
+
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    //id serial primary key,
+    //area_name text,
+    //hotel_name text,
+    //address text,
+    //nearest_station text,
+    //price integer,
+    //parking text
+
+    private static final RowMapper<searchHotel> SEARCHHOTEL_ROW_MAPPER = new BeanPropertyRowMapper<>(searchHotel.class);
+
+    /**
+     * 引数で渡した金額以下のホテル一覧を表示するメソッド
+     * @param price
+     * @return 条件に一致するホテルの全情報
+     */
+    public List<searchHotel> searchByLessThanPrice(Integer price){
+        String sql = "SELECT hotel_name,nearest_station,price FROM hotels WHERE price=:price";
+        return null;
+    }
     
 }
