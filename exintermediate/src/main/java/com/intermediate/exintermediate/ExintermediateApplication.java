c95ec46326164b83cba4972d7d3ce42151e1dfa1
kotaro <kotaro.matsuhana@rakus-partners.co.jp>
2026-02-16 09:55:17 +0900

    最初のコミッto

---
+++ exintermediate/src/main/java/com/intermediate/exintermediate/ExintermediateApplication.java
@@ -0,0 +1,13 @@
+package com.intermediate.exintermediate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExintermediateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExintermediateApplication.class, args);
+	}
+
+}
---
+++ exintermediate/src/test/java/com/intermediate/exintermediate/ExintermediateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intermediate.exintermediate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExintermediateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
